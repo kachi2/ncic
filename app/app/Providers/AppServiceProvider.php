7fab08853d649c368cb3ee641a1489e11321cabd
Michael Kachi <jesmikky@gmail.com>
2023-06-16 18:14:11 +0100

    updates

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -27,13 +27,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->composer('*', function($view){
-        $menus = Menu::where('status', 1)->get();
-        $view->with('menus', $menus);
+    //     view()->composer('*', function($view){
+    //     $menus = Menu::where('status', 1)->get();
+    //     $view->with('menus', $menus);
 
-        $view->with('settings', Setting::latest()->first());
-        });
-       view::share('unread_notify', AdminNotify::where('status', 0)->latest()->take(10)->get());
-       view::share('read_notify', AdminNotify::where('status', 1)->latest()->take(2)->get());
+    //     $view->with('settings', Setting::latest()->first());
+    //     });
+    //    view::share('unread_notify', AdminNotify::where('status', 0)->latest()->take(10)->get());
+    //    view::share('read_notify', AdminNotify::where('status', 1)->latest()->take(2)->get());
     }
 }
